db5d7e7390fc06f6317fb0081766a61c51c02cac
EnmaSantos <del20047@byui.edu>
2025-03-14 15:54:15 -0600

    refactor server setup and routing for improved structure and clarity

--- server.js
+++ server.js
@@ -1,72 +1,31 @@
-// Get dependencies
-var express = require('express');
-var path = require('path');
-var http = require('http');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
 
-// import the routing file to handle the default (index) route
-var index = require('./server/routes/app');
+// API file for interacting with MongoDB
+const appRoutes = require('./server/routes/app');
 
-// ... ADD CODE TO IMPORT YOUR ROUTING FILES HERE ... 
-
-var app = express(); // create an instance of express
-
-// Tell express to use the following parsers for POST data
+// Parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(logger('dev')); // Tell express to use the Morgan logger
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist/cms-project')));
 
-// Add support for CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-  );
-  next();
-});
+// API location
+app.use('/', appRoutes);
 
-// Tell express to use the specified director as the
-// root directory for your web site
-app.use(express.static(path.join(__dirname, 'dist/cms')));
-
-// Tell express to map the default route ('/') to the index route
-app.use('/', index);
-
-// Tell express to map the default route ('/') to the index route
-app.use('/', index);
-
-// Tell express to map all other non-defined routes back to the index page
-// catch 404 and forward to error handler
-
-app.use(function(req, res, next) {
-  res.render('index');
-});
-
-
-// Tell express to map all other non-defined routes back to the index page
+// Send all other requests to the Angular app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/cms/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/cms-project/browser/index.html'));
 });
 
-// Define the port address and tell express to use this port
+// Set Port
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
-// Create HTTP server.
 const server = http.createServer(app);
 
-// Tell the server to start listening on the provided port
-server.listen(port, function() {
-  console.log('API running on localhost: ' + port)
-});
+server.listen(port, () => console.log(`Running on localhost:${port}`));
--- server/routes/app.js
+++ server/routes/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path'); // Add this import
 var router = express.Router();
 
 /* GET home page. */
